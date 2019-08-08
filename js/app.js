c40c6d8579267071033a48db411d0c24bcc5f87a
Biniam <biniamt23@yahoo.com>
2019-08-08 14:32:25 -0700

    added top 10 movies list

--- js/app.js
+++ js/app.js
@@ -2,14 +2,14 @@
 var score = 0;
 
 var name = prompt('Hello, What is your name?')
-console.log(name);
+console.log('user\'s name is:', name);
 alert('Hello ' + name + ' nice to meet you! My name is Biniam.')
 var game = prompt('Would you like to play a guessing game in order to view my site? Yes/No').toLowerCase();
 if (game === 'yes') {
     alert('Alright, click ok to start')
 }
 else {
-    alert('To bad we\'re playing!')
+    alert('Too bad we\'re playing.')
 }
 
 // FIRST QUESTION
@@ -20,15 +20,15 @@ if (age != '23') {
 }
 else {
     alert('Correct, next question.')
-    score+=1
+    score += 1
 }
 
 //SECOND QUESTION
 var city = prompt('Guess what city I\'m from.').toLowerCase();
-console.log(city);
+console.log('user entered:', city);
 if (city === 'seattle') {
     alert('Correct, next question.')
-    score+=1
+    score += 1
 }
 else {
     alert('Incorrect, let\'s see if you can get the next one.')
@@ -36,10 +36,10 @@ else {
 
 //THIRD QUESTION
 var color = prompt('Guess my favorite color. Hint: It\'s either blue, red, green, or purple.').toLowerCase();
-console.log(color);
+console.log('user entered', color);
 if (color === 'blue') {
     alert('Correct, moving on.')
-    score+=1
+    score += 1
 }
 else {
     alert('Nope, next question.')
@@ -47,10 +47,10 @@ else {
 
 //FOURTH QUESTION
 var sib = prompt('Guess how many siblings I have. Hint: Between 1-5.')
-console.log(sib);
+console.log('sibling\'s guess', sib);
 if (sib === '4') {
     alert('Correct.')
-    score+=1
+    score += 1
 }
 else {
     alert('Nope.')
@@ -58,40 +58,40 @@ else {
 
 //FIFTH QUESTION
 var food = prompt('Guess what my favorite food is. Hint: It\'s either pizza, burgers, or lasagna.').toLowerCase();
-console.log(food);
+console.log('user typed:', food);
 if (food === 'lasagna') {
     alert('Correct.')
-    score+=1
+    score += 1
 }
 else {
     alert('Sorry that\'s incorrect.')
 }
 
 //SIXTH QUESTION
-    for (var i = 0; i <= 3; i++) { //give the user three tries to guess correctly.
-        var q6 = prompt('Guess my favorite number. You have 4 tries. Hint it\'s between 1-10.');
-        console.log('Guessed:', q6);
-        i = parseInt(i);
-        console.log('g', i);
-        if (q6 > 2) {
-            alert('Too high.')
-        }
-        else if (q6 < 2) {
-            alert('Too low.')
-        }
-        else {
-            alert('Correct.')
-            score+=1 //score must go before the break
-            break;
-        }
+for (var i = 0; i <= 3; i++) { //give the user three tries to guess correctly.
+    var q6 = prompt('Guess my favorite number. You have 4 tries. Hint it\'s between 1-10.');
+    console.log('Guessed:', q6);
+    i = parseInt(i);
+    console.log('g', i);
+    if (q6 > 2) {
+        alert('Too high.')
     }
+    else if (q6 < 2) {
+        alert('Too low.')
+    }
+    else {
+        alert('Correct.')
+        score += 1 //score must go before the break
+        break;
+    }
+}
 
 //SEVENTH QUESTION
 var city = ['MIAMI', 'SEATTLE', 'DALLAS', 'HOUSTON', 'OAKLAND', 'BROOKLYN']
 var tries = 0; //the amount of tries a user has is set to 0 first.
 var ind; //ind = indicator
 
-while (tries <=6 ) {
+while (tries <= 6) {
     var q7 = prompt("Can you guess one of my favorite cities in the US. You have 7 tries.").toUpperCase();
     for (var j = 0; j < city.length; j++) {
         if (q7 === city[j]) {
@@ -103,15 +103,18 @@ while (tries <=6 ) {
         tries += 1;
     } else { // otherwise, if the user's input matches any city, then alert correct
         score += 1 // have to give them a score before the alert
-        tries+=7
-        alert('Correct, one of my favorite cities is ' + q7 + '. My favorite cities are MIAMI, SEATTLE, DALLAS, HOUSTON, OAKLAND, BROOKLYN.')
+        tries += 7
+        alert('Correct, one of my favorite cities is ' + q7 + '. My favorite cities are MIAMI, SEATTLE, DALLAS, HOUSTON, OAKLAND, and BROOKLYN.')
     }
     if (tries === 6) {
-        alert('Sorry you got that last question wrong. So, ' + name + ' you got ' + score + ' out of 7 on my questions.')
+        alert('Sorry ' + name + ' you ran out of tries.')
         break;
-
     }
 
 }
-alert('So, ' + name + ' you got ' + score + ' out of 7 on my questions.') // opens last window stating this.
-
+if (score >= 5) {
+    alert('So, ' + name + ' you got ' + score + ' out of 7 on my questions. Great Job!') // opens last window stating this.
+}
+else {
+    alert('So, ' + name + ' you only got ' + score + ' out of 7 on my questions. Try harder next time.') // opens last window stating this.
+}
