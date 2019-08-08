9a142d86ab3558e34a274e2ca9f1280703bf24c6
Ethan Davis <davisethan93@gmail.com>
2019-08-08 16:49:58 -0700

    Questions 1 - 7 are now in functions

--- js/app.js
+++ js/app.js
@@ -13,105 +13,159 @@ else {
 }
 
 // FIRST QUESTION
-var age = prompt('Guess my age.')
-console.log('my age', age);
-if (age != '23') {
-    alert('Incorrect, next question.')
-}
-else {
-    alert('Correct, next question.')
-    score += 1
-}
 
-//SECOND QUESTION
-var city = prompt('Guess what city I\'m from.').toLowerCase();
-console.log('user entered:', city);
-if (city === 'seattle') {
-    alert('Correct, next question.')
-    score += 1
-}
-else {
-    alert('Incorrect, let\'s see if you can get the next one.')
-}
-
-//THIRD QUESTION
-var color = prompt('Guess my favorite color. Hint: It\'s either blue, red, green, or purple.').toLowerCase();
-console.log('user entered', color);
-if (color === 'blue') {
-    alert('Correct, moving on.')
-    score += 1
-}
-else {
-    alert('Nope, next question.')
-}
-
-//FOURTH QUESTION
-var sib = prompt('Guess how many siblings I have. Hint: Between 1-5.')
-console.log('sibling\'s guess', sib);
-if (sib === '4') {
-    alert('Correct.')
-    score += 1
-}
-else {
-    alert('Nope.')
-}
-
-//FIFTH QUESTION
-var food = prompt('Guess what my favorite food is. Hint: It\'s either pizza, burgers, or lasagna.').toLowerCase();
-console.log('user typed:', food);
-if (food === 'lasagna') {
-    alert('Correct.')
-    score += 1
-}
-else {
-    alert('Sorry that\'s incorrect.')
-}
-
-//SIXTH QUESTION
-for (var i = 0; i <= 3; i++) { //give the user three tries to guess correctly.
-    var q6 = prompt('Guess my favorite number. You have 4 tries. Hint it\'s between 1-10.');
-    console.log('Guessed:', q6);
-    i = parseInt(i);
-    console.log('g', i);
-    if (q6 > 2) {
-        alert('Too high.')
-    }
-    else if (q6 < 2) {
-        alert('Too low.')
+function questionOne(score) {
+    var age = prompt('Guess my age.')
+    console.log('my age', age);
+    if (age != '23') {
+        alert('Incorrect, next question.')
     }
     else {
-        alert('Correct.')
-        score += 1 //score must go before the break
-        break;
+        alert('Correct, next question.')
+        score += 1
     }
+
+    return score;
 }
 
-//SEVENTH QUESTION
-var city = ['MIAMI', 'SEATTLE', 'DALLAS', 'HOUSTON', 'OAKLAND', 'BROOKLYN']
-var tries = 0; //the amount of tries a user has is set to 0 first.
-var ind; //ind = indicator
+score = questionOne(score);
 
-while (tries <= 6) {
-    var q7 = prompt("Can you guess one of my favorite cities in the US. You have 7 tries.").toUpperCase();
-    for (var j = 0; j < city.length; j++) {
-        if (q7 === city[j]) {
-            ind = true //if true then is correct
+// SECOND QUESTION
+
+function questionTwo(score) {
+    var city = prompt('Guess what city I\'m from.').toLowerCase();
+    console.log('user entered:', city);
+    if (city === 'seattle') {
+        alert('Correct, next question.')
+        score += 1
+    }
+    else {
+        alert('Incorrect, let\'s see if you can get the next one.')
+    }
+
+    return score;
+}
+
+score = questionTwo(score);
+
+// THIRD QUESTION
+
+function questionThree(score) {
+    var color = prompt('Guess my favorite color. Hint: It\'s either blue, red, green, or purple.').toLowerCase();
+    console.log('user entered', color);
+    if (color === 'blue') {
+        alert('Correct, moving on.')
+        score += 1
+    }
+    else {
+        alert('Nope, next question.')
+    }
+
+    return score;
+}
+
+score = questionThree(score);
+
+// FOURTH QUESTION
+
+function questionFour(score) {
+    var sib = prompt('Guess how many siblings I have. Hint: Between 1-5.')
+    console.log('sibling\'s guess', sib);
+    if (sib === '4') {
+        alert('Correct.')
+        score += 1
+    }
+    else {
+        alert('Nope.')
+    }
+
+    return score;
+}
+
+score = questionFour(score);
+
+// FIFTH QUESTION
+
+function questionFive(score) {
+    var food = prompt('Guess what my favorite food is. Hint: It\'s either pizza, burgers, or lasagna.').toLowerCase();
+    console.log('user typed:', food);
+    if (food === 'lasagna') {
+        alert('Correct.')
+        score += 1
+    }
+    else {
+        alert('Sorry that\'s incorrect.')
+    }
+
+    return score;
+}
+
+score = questionFive(score);
+
+// SIXTH QUESTION
+
+function questionSix(score) {
+    for (var i = 0; i <= 3; i++) { //give the user three tries to guess correctly.
+        var q6 = prompt('Guess my favorite number. You have 4 tries. Hint it\'s between 1-10.');
+        console.log('Guessed:', q6);
+        i = parseInt(i);
+        console.log('g', i);
+        if (q6 > 2) {
+            alert('Too high.')
+        }
+        else if (q6 < 2) {
+            alert('Too low.')
+        }
+        else {
+            alert('Correct.')
+            score += 1 //score must go before the break
+            break;
         }
     }
-    if (!ind) { // if indicator is false run alert and add a try
-        alert('Nope, try again.')
-        tries += 1;
-    } else { // otherwise, if the user's input matches any city, then alert correct
-        score += 1 // have to give them a score before the alert
-        tries += 7
-        alert('Correct, one of my favorite cities is ' + q7 + '. My favorite cities are MIAMI, SEATTLE, DALLAS, HOUSTON, OAKLAND, and BROOKLYN.')
-    }
-    if (tries === 6) {
-        alert('Sorry ' + name + ' you ran out of tries.')
-        break;
+
+    return score;
+}
+
+score = questionSix(score);
+
+// SEVENTH QUESTION
+
+function questionSeven(score) {
+    var city = ['MIAMI', 'SEATTLE', 'DALLAS', 'HOUSTON', 'OAKLAND', 'BROOKLYN']
+    var tries = 0; //the amount of tries a user has is set to 0 first.
+    var ind; //ind = indicator
+    
+    while (tries <= 6) {
+        var q7 = prompt("Can you guess one of my favorite cities in the US. You have 7 tries.").toUpperCase();
+        for (var j = 0; j < city.length; j++) {
+            if (q7 === city[j]) {
+                ind = true //if true then is correct
+            }
+        }
+        if (!ind) { // if indicator is false run alert and add a try
+            alert('Nope, try again.')
+            tries += 1;
+        } else { // otherwise, if the user's input matches any city, then alert correct
+            score += 1 // have to give them a score before the alert
+            tries += 7
+            alert('Correct, one of my favorite cities is ' + q7 + '. My favorite cities are MIAMI, SEATTLE, DALLAS, HOUSTON, OAKLAND, and BROOKLYN.')
+        }
+        if (tries === 6) {
+            alert('Sorry ' + name + ' you ran out of tries.')
+            break;
+        }
+    
     }
 
+    return score;
 }
+
+score = questionSeven(score);
+
+/**
+ * Score alert
+ */
+
 if (score >= 5) {
     alert('So, ' + name + ' you got ' + score + ' out of 7 on my questions. Great Job!') // opens last window stating this.
 }
